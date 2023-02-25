47ebaf0d18c7718f49e0c579dd3961c3756905df
Ashary Vermaysha <asharyver13@gmail.com>
2023-02-25 12:19:59 +0700

    Rename package name and update install command

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Vermaysha\LaravelWilayahID;
 
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Vermaysha\LaravelWilayahID\Commands\SeedCommand;
@@ -16,14 +17,24 @@ class ServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('laravel-wilayah-indonesia')
-            ->hasConfigFile()
+            ->name('laravel-wilayah-id')
+            ->hasConfigFile('wilayah-id')
             ->hasMigrations([
                 'create_provinces_table',
                 'create_cities_table',
                 'create_districts_table',
                 'create_village_table',
             ])
-            ->hasCommand(SeedCommand::class);
+            ->hasCommand(SeedCommand::class)
+            ->hasInstallCommand(function (InstallCommand $command) {
+                $command
+                    ->publishConfigFile()
+                    ->publishMigrations()
+                    ->askToRunMigrations()
+                    ->askToStarRepoOnGitHub('vermaysha/laravel-wilayah-indonesia')
+                    ->endWith(function (InstallCommand $command) {
+                        $command->info('Please run `php artisan wilayah-id:seed` to produce data');
+                    });
+            });
     }
 }
