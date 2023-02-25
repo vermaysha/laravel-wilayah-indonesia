e3c0b3cd29da7cc1567dbec22d8217355bc5ddb7
Ashary Vermaysha <asharyver13@gmail.com>
2023-02-25 11:18:40 +0700

    Add test for Village model to check district data relationship

--- tests/ModelTests/VillageTest.php
+++ tests/ModelTests/VillageTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use Vermaysha\LaravelWilayahID\Models\District;
 use Vermaysha\LaravelWilayahID\Models\Village;
+use Vermaysha\LaravelWilayahID\Seeds\DistrictSeeder;
 use Vermaysha\LaravelWilayahID\Seeds\VillageSeeder;
 
 it('village has data', function () {
@@ -9,4 +11,15 @@ it('village has data', function () {
     $village = Village::first();
 
     expect($village->toArray())->toHaveKeys(['code', 'district_code', 'name', 'created_at', 'updated_at']);
+});
+
+it('village has district data', function () {
+    $this->seed([
+        DistrictSeeder::class,
+        VillageSeeder::class
+    ]);
+
+    $village = Village::with('district')->first();
+
+    $this->assertInstanceOf(District::class, $village->district);
 });
