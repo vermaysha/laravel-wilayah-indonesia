40d70d7305f76716f13b7b2c54d5f4a6135f21bd
Ashary Vermaysha <asharyver13@gmail.com>
2023-02-25 11:18:33 +0700

    Add relationship method for District model in Village model

--- src/Models/Village.php
+++ src/Models/Village.php
@@ -2,7 +2,16 @@
 
 namespace Vermaysha\LaravelWilayahID\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Village extends Model
 {
-    //
+    public function district(): BelongsTo
+    {
+        return $this->belongsTo(
+            District::class,
+            'district_code',
+            'code'
+        );
+    }
 }
