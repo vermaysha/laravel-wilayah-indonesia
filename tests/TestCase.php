4b24ecbbd898b6b1ec0a46d6fc6c33740fbc21fb
Ashary Vermaysha <asharyver13@gmail.com>
2023-02-25 08:33:28 +0700

    Update ServiceProvider and run migrations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,7 @@ namespace Vermaysha\LaravelWilayahIndonesia\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Vermaysha\LaravelWilayahIndonesia\LaravelWilayahIndonesiaServiceProvider;
+use Vermaysha\LaravelWilayahIndonesia\ServiceProvider;
 
 class TestCase extends Orchestra
 {
@@ -20,7 +20,7 @@ class TestCase extends Orchestra
     protected function getPackageProviders($app)
     {
         return [
-            LaravelWilayahIndonesiaServiceProvider::class,
+            ServiceProvider::class,
         ];
     }
 
@@ -28,9 +28,17 @@ class TestCase extends Orchestra
     {
         config()->set('database.default', 'testing');
 
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-wilayah-indonesia_table.php.stub';
-        $migration->up();
-        */
+        $migrationsFiles = [
+            'create_provinces_table.php.stub',
+            'create_cities_table.php.stub',
+            'create_districts_table.php.stub',
+            'create_village_table.php.stub',
+        ];
+
+        foreach ($migrationsFiles as $file) {
+            $migration = include __DIR__.'/../database/migrations/' . $file;
+            $migration->up();
+        }
+
     }
 }
