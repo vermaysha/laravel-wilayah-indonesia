b4c36e6d21e87ba67e1b3a6682ce6f8f8a1e2c4f
Ashary Vermaysha <asharyver13@gmail.com>
2023-02-25 11:12:21 +0700

    Add relationships to City model

--- src/Models/City.php
+++ src/Models/City.php
@@ -2,7 +2,39 @@
 
 namespace Vermaysha\LaravelWilayahID\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+
 class City extends Model
 {
-    //
+    public function province(): BelongsTo
+    {
+        return $this->belongsTo(
+            Province::class,
+            'province_code',
+            'code'
+        );
+    }
+
+    public function districts(): HasMany
+    {
+        return $this->hasMany(
+            District::class,
+            'city_code',
+            'code'
+        );
+    }
+
+    public function villages(): HasManyThrough
+    {
+        return $this->hasManyThrough(
+            Village::class,
+            District::class,
+            'city_code',
+            'district_code',
+            'code',
+            'code'
+        );
+    }
 }
