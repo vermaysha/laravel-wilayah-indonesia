efa11b627cb6d649b104c1ab651dea93b1193b9b
Ashary Vermaysha <asharyver13@gmail.com>
2023-02-25 10:06:59 +0700

    Refactor code to remove dependency on Eloquent Factories

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Vermaysha\LaravelWilayahID\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Vermaysha\LaravelWilayahID\ServiceProvider;
 
